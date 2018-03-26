028d0ddd6121bbe6f514bf59e1b014bbf519f1da
Daniel Wojtowicz <dwojtowi@hawk.iit.edu>
2018-03-26 02:05:01 -0500

    Add LazyPoller and PollResult

---
+++ src/LazyPoller.java
@@ -0,0 +1,26 @@
+/**
+ * THe LazyPoller only is created and run on a separate thread when PULL mode is activated. It calls the peer's
+ * lazy poll method every period of time, which is a provided constant in this file.
+ */
+public class LazyPoller {
+  private static final int LAZY_POLL_FREQUENCY = 1000 * 30; // How often to poll, defaulted at every 30 seconds
+  private Peer peer;
+
+  public LazyPoller(Peer peer) {
+    this.peer = peer;
+  }
+
+  /**
+   * The method that a thread will run. Runs the peer's lazy-polling feature and then sleeps.
+   */
+  public void autoPoll(){
+    while(true){
+      peer.lazyPoll();
+      try {
+        Thread.sleep(LAZY_POLL_FREQUENCY);
+      } catch (Exception e){
+        e.printStackTrace();
+      }
+    }
+  }
+}
---
+++ src/PollResult.java
@@ -0,0 +1,35 @@
+/**
+ * This class is used for Polling messages. It is a self explanatory container used to convey if a file is out of date
+ * and if not, provide a new TTR for the file to update on.
+ */
+public class PollResult {
+  private boolean outOfDate;
+  private long newTTR;
+
+  // ****************
+  // Boilerplate Code
+  // ****************
+
+  // Comments omitted because this is generic, boilerplate code
+
+  public PollResult(boolean outOfDate, long newTTR) {
+    this.outOfDate = outOfDate;
+    this.newTTR = newTTR;
+  }
+
+  public boolean isOutOfDate() {
+    return outOfDate;
+  }
+
+  public void setOutOfDate(boolean outOfDate) {
+    this.outOfDate = outOfDate;
+  }
+
+  public long getNewTTR() {
+    return newTTR;
+  }
+
+  public void setNewTTR(long newTTR) {
+    this.newTTR = newTTR;
+  }
+}
